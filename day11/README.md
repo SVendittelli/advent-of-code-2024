429d475689eaa7019400cfe8ed15b57c79a248ec
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-12-11 12:43:06 +0000

    feat(day11): solve part 2

--- day11/README.md
+++ day11/README.md
@@ -7,4 +7,9 @@ https://adventofcode.com/2024/day/11
 Results in seconds:
 
 ```json
+{
+  "avg": 0.012566035340425533,
+  "min": 0.010610885,
+  "max": 0.015169751
+}
 ```
--- day11/index.ts
+++ day11/index.ts
@@ -1,32 +1,88 @@
 import type { Run } from "~/utils/types";
 import { readWholeFile } from "~/utils";
 
+const numDigits = (num: number): number => {
+  return (Math.log(num) * Math.LOG10E + 1) | 0;
+};
+const firstHalf = (num: number, length: number): number => {
+  return Math.floor(num / Math.pow(10, length / 2));
+};
+const secondHalf = (num: number, length: number): number => {
+  return num % Math.pow(10, length / 2);
+};
+const addToCount = <K>(
+  oldMap: Map<K, number>,
+  newMap: Map<K, number>,
+  oldValue: K,
+  newValue: K,
+) => {
+  let newCount = (newMap.get(newValue) ?? 0) + (oldMap.get(oldValue) ?? 0);
+  newMap.set(newValue, newCount);
+};
+
 const run: Run = async () => {
   const filePath = "day11/input.txt";
   const input = (await readWholeFile(filePath))
     .split(" ")
     .map((stone) => +stone);
 
-  const apply = (stone: number): number | number[] => {
-    const asString = `${stone}`;
-    if (stone === 0) {
-      return stone + 1;
-    } else if (asString.length % 2 === 0) {
-      return [
-        +asString.substring(0, asString.length / 2),
-        +asString.substring(asString.length / 2),
-      ];
-    } else {
-      return stone * 2024;
+  /** Count of total stones with a given value */
+  let countByValue: Map<number, number> = new Map();
+  /** Set of the different values of stones */
+  let stones: Set<number> = new Set();
+
+  input.forEach((stone) => {
+    countByValue.set(stone, 1);
+    stones.add(stone);
+  });
+
+  const iterate = (depth: number) => {
+    for (let i = 0; i < depth; ++i) {
+      const newCountByValue: Map<number, number> = new Map();
+      const newStones: Set<number> = new Set();
+
+      for (let stone of stones) {
+        if (stone === 0) {
+          const value = 1;
+          newStones.add(value);
+          addToCount(countByValue, newCountByValue, stone, value);
+          continue;
+        }
+
+        const length = numDigits(stone);
+        if (length % 2 === 0) {
+          const first = firstHalf(stone, length);
+          const second = secondHalf(stone, length);
+
+          newStones.add(first);
+          addToCount(countByValue, newCountByValue, stone, first);
+
+          newStones.add(second);
+          addToCount(countByValue, newCountByValue, stone, second);
+          continue;
+        }
+
+        const value = stone * 2024;
+        newStones.add(value);
+        addToCount(countByValue, newCountByValue, stone, value);
+      }
+
+      countByValue = newCountByValue;
+      stones = newStones;
     }
   };
 
-  let stones = input;
-  for (let i = 0; i < 25; ++i) {
-    stones = stones.map(apply).flat();
-  }
+  iterate(25);
+  const part1 = stones
+    .values()
+    .reduce((acc, stone) => acc + (countByValue.get(stone) ?? 0), 0);
 
-  return [stones.length, 0];
+  iterate(75 - 25);
+  const part2 = stones
+    .values()
+    .reduce((acc, stone) => acc + (countByValue.get(stone) ?? 0), 0);
+
+  return [part1, part2];
 };
 
 export default run;
