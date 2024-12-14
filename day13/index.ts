11609cd3c8ce102d83a019500237d66997efa084
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-12-14 08:32:47 +0000

    feat(day13): solve part 2

--- day13/index.ts
+++ day13/index.ts
@@ -1,11 +1,14 @@
 import type { Run } from "~/utils/types";
 import { readWholeFile } from "~/utils";
 
+type Coefficients = { x: number; y: number };
+type Problem = { a: Coefficients; b: Coefficients; total: Coefficients };
+
 const run: Run = async () => {
   const filePath = "day13/input.txt";
   const input = await readWholeFile(filePath);
 
-  const problems = input.split("\n\n").map((group) => {
+  const problems: Problem[] = input.split("\n\n").map((group) => {
     const [a, b, total] = group
       .split("\n")
       .map((line) => line.split(": ")[1])
@@ -17,19 +20,34 @@ const run: Run = async () => {
     return { a, b, total };
   });
 
-  const { aTotal: a, bTotal: b } = problems.reduce(
-    (acc, { a, b, total }) => {
-      const aCount = (b.y * total.x - b.x * total.y) / (b.y * a.x - b.x * a.y);
-      const bCount = (total.x - a.x * aCount) / b.x;
+  const reducer = (
+    acc: { aTotal: number; bTotal: number },
+    { a, b, total }: Problem,
+  ): { aTotal: number; bTotal: number } => {
+    const aCount = (b.y * total.x - b.x * total.y) / (b.y * a.x - b.x * a.y);
+    const bCount = (total.x - a.x * aCount) / b.x;
 
-      return Number.isInteger(aCount) && Number.isInteger(bCount)
-        ? { aTotal: acc.aTotal + aCount, bTotal: acc.bTotal + bCount }
-        : acc;
-    },
-    { aTotal: 0, bTotal: 0 },
-  );
+    return Number.isInteger(aCount) && Number.isInteger(bCount)
+      ? { aTotal: acc.aTotal + aCount, bTotal: acc.bTotal + bCount }
+      : acc;
+  };
 
-  return [3 * a + b, 0];
+  const { aTotal: a1, bTotal: b1 } = problems.reduce(reducer, {
+    aTotal: 0,
+    bTotal: 0,
+  });
+  const { aTotal: a2, bTotal: b2 } = problems
+    .map(({ a, b, total: { x, y } }) => ({
+      a,
+      b,
+      total: { x: x + 10000000000000, y: y + 10000000000000 },
+    }))
+    .reduce(reducer, {
+      aTotal: 0,
+      bTotal: 0,
+    });
+
+  return [3 * a1 + b1, 3 * a2 + b2];
 };
 
 export default run;
